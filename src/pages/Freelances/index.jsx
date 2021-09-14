a7e51192be850cd8696d192379cc0c3a548ba6d7
fbergaut <f.bergaut@gmail.com>
2021-09-14 14:41:25 +0200

    Apprentissage de react-router

---
+++ src/pages/Freelances/index.jsx
@@ -0,0 +1,9 @@
+function Freelances() {
+  return (
+    <div>
+      <h1> Florence Bergaut 🙎</h1>
+    </div>
+  )
+}
+
+export default Freelances
---
+++ src/pages/Results/index.jsx
@@ -0,0 +1,9 @@
+function Results() {
+  return (
+    <div>
+      <h1> Trouvez votre prestataire 🙎</h1>
+    </div>
+  )
+}
+
+export default Results
