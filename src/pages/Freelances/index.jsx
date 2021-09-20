5dccc20bd908326992bbb840a330bcf65921cec6
fbergaut <f.bergaut@gmail.com>
2021-09-20 15:12:16 +0200

    Afficher les datas d'une API dans un composant classe

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,9 +1,9 @@
-// import { useState, useEffect } from 'react'
 import Card from '../../components/Card'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
 import { useFetch, useTheme } from '../../utils/hooks'
+import { Link } from 'react-router-dom'
 
 const CardsContainer = styled.div`
   display: grid;
@@ -16,9 +16,9 @@ const CardsContainer = styled.div`
 
 const PageTitle = styled.h1`
   font-size: 30px;
-  color: black;
   text-align: center;
   padding-bottom: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const PageSubtitle = styled.h2`
@@ -27,75 +27,13 @@ const PageSubtitle = styled.h2`
   font-weight: 300;
   text-align: center;
   padding-bottom: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
+
 const LoaderWrapper = styled.div`
   display: flex;
   justify-content: center;
 `
-
-// function Freelances() {
-//   const [freelancersList, setfreelancersList] = useState([])
-//   const [isDataLoading, setDataLoading] = useState(false)
-  // const [error, setError] = useState(false)
-
-  // useEffect(() => {
-  //   async function fetchFreelances() {
-  //     setDataLoading(true)
-  //     try {
-  //       const response = await fetch(`http://localhost:8000/freelances`)
-  //       const { freelancersList } = await response.json()
-  //       setFreelancesList(freelancersList)
-  //     } catch (err) {
-  //       console.log('===== error =====', err)
-  //       setError(true)
-  //     } finally {
-  //       setDataLoading(false)
-  //     }
-  //   }
-  //   fetchFreelances()
-  // }, [])
-
-  // if (error) {
-  //   return <span>Oups il y a eu un problème</span>
-//   // }
-
-//   useEffect(() => {
-//     setDataLoading(true)
-//     fetch(`http://localhost:8000/freelances`).then((response) =>
-//       response
-//         .json()
-//         .then(({ freelancersList }) => {
-//           setfreelancersList(freelancersList)
-//           setDataLoading(false)
-//         })
-//         .catch((error) => console.log(error))
-//     )
-//   }, [])
-
-//   return (
-//     <div>
-//       <PageTitle>Trouvez votre prestataire</PageTitle>
-//       <PageSubtitle>
-//         Chez Shiny nous réunissons les meilleurs profils pour vous.
-//       </PageSubtitle>
-//       {isDataLoading ? (
-//       <LoaderWrapper>
-//           <Loader />
-//       </LoaderWrapper>) : (
-//       <CardsContainer>
-//         {freelancersList.map((profile, index) => (
-//           <Card
-//             key={`${profile.name}-${index}`}
-//             label={profile.job}
-//             title={profile.name}
-//             picture={profile.picture}
-//           />
-//         ))}
-//       </CardsContainer>
-//       )}
-//     </div>
-//   )
-// }
 
 function Freelances() {
   const { theme } = useTheme()
@@ -103,13 +41,10 @@ function Freelances() {
     `http://localhost:8000/freelances`
   )
 
-  // Ici le "?" permet de s'assurer que data existe bien.
-  // Vous pouvez en apprendre davantage sur cette notation ici :
-  // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Optional_chaining
   const freelancersList = data?.freelancersList
 
   if (error) {
-    return <span>Oups il y a eu un problème</span>
+    return <span>Il y a un problème</span>
   }
 
   return (
@@ -124,13 +59,15 @@ function Freelances() {
         </LoaderWrapper>
       ) : (
         <CardsContainer>
-          {freelancersList.map((profile, index) => (
-            <Card
-              key={`${profile.name}-${index}`}
-              label={profile.job}
-              title={profile.name}
-              picture={profile.picture}
-            />
+          {freelancersList?.map((profile) => (
+            <Link key={`freelance-${profile.id}`} to={`/profile/${profile.id}`}>
+              <Card
+                label={profile.job}
+                title={profile.name}
+                picture={profile.picture}
+                theme={theme}
+              />
+            </Link>
           ))}
         </CardsContainer>
       )}
